1076a9e8995ea284cde574828182df2116f1cc31
tlee8 <tlee8@stuy.edu>
2019-01-30 20:29:08 -0500

    added k00

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,13 +1,44 @@
+//Thomas Lee
+//SoftDev2 pd6
+//K00 -- I See A Red Door
+//2019-01-31
+
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
-ctx.fillRect(50,50,100,200)
+var state = "box";
 
-var clear = function() {
-    ctx.clearRect(0,0,600,600);
+var clearCanvas = function() {
+    ctx.clearRect(0, 0, c.width, c.height);
+};
+
+var draw = function(event) {
+    var border = c.getBoundingClientRect();
+    var x = event.clientX - border.left;
+    var y = event.clientY - border.top;
+    if (state == "box"){
+	ctx.fillStyle = "#ff0000"
+	ctx.fillRect(x, y, 50, 100);
+    }
+    else{
+	ctx.fillStyle = "#ffff00"
+	ctx.beginPath();
+	ctx.ellipse(x, y, 5, 5, 0, 0, 2 * Math.PI);
+	ctx.fill();
+    }
 }
 
-var clearButt = getElementById("clear");
+var changeState = function() {
+    if (state == "box"){
+	state = "dot";
+    }
+    else{
+	state = "box"
+    }
+    console.log(state)
+}
 
-clearButt.addEventListener("click", clear);
+var clear = document.getElementById("clear");
+var change = document.getElementById("change");
 
-
+clear.addEventListener("click", clearCanvas)
+change.addEventListener("click", changeState)
