b1286239540d38601c812391685f4c213600845a
tlee8 <tlee8@stuy.edu>
2019-03-08 07:00:55 -0500

    finish

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -3,33 +3,44 @@
 #K08 -- Ay Mon, Go Git It From Yer Flask
 #2019-03-08
 
-from flask import Flask, redirect, url_for, render_template, session, request
+from flask import Flask,render_template,request,session
+#import mongo
 import pymongo
-import os
 import json
+import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
-collection = None  
+collection=None
+db=None
+ip = 157.230.50.34
 
-global SERVER_ADDR = "134.209.67.113"
-
-@app.route('/')
+@app.route("/")
 def home():
-	return render_template('home.html')
+    return render_template("home.html")
 
-@app.route("/build", methods = ["POST"])
-def build():
-    SERVER_ADDR = request.form['serverid']
-	connection=pymongo.MongoClient(SERVER_ADDR)
-	db=connection.Liusers
-	collection=db.movies
-	f = open("movies.json","r")
-	data = json.loads(f.read())
-	f.close()
-	collection.insert_many(data)
-    queryResults=None
-return render_template('home.html', queryResults = queryResults) 
+@app.route("/get_ip")
+def get_ip():
+    if request.args.get("ip") != None:
+        try:
+            ip = request.args.get("ip")
+            connection = pymongo.MongoClient(ip)
+            connection.drop_database("database")
+            db = connection["BoatBoatInspire"]
+            collection = db["movies"]
+            file = open('movies.json')
+            data = json.load(F)
+            collection.insert_many(data)
+        except:
+            ip = 157.230.50.34
+            connection = pymongo.MongoClient(ip)
+            connection.drop_database("database")
+            db = connection["BoatBoatInspire"]
+            collection = db["movies"]
+            file = open('movies.json')
+            data = json.load(F)
+            collection.insert_many(data)
+    return render_template("search.html",ip=ip)
 
 def title_movie(title):
     return collection.find({'title':title})
@@ -40,16 +51,14 @@ def genre_movie(genre):
 def year_movie(year):
     return collection.find({'year':year})
 
-def lessthan_year(year):
-    return collection.find({'year':{'$lt':year}})
+@app.route("/query")
+def query():
+    results=[]
+    q = request.args.get("search")
+    results.append(title_movie(q))
+    results.append(genre_movie(q))
+    results.append(year_movie(q))
+    return render_template("results.html",results=results)
 
-def year_and_genre(year,genre):
-    return collection.find({'year': year, 'genre': genre})
-
-def display(collection):
-    for i in collection:
-        print(i['title'])
-
-if __name__ == "__main__":
-    app.debug = True
+app.debug = True
 app.run()
