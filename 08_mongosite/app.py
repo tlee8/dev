81bbe16162d721c85a2bf1ff066ce990af31a357
Thomas Lee <34113032+tlee8@users.noreply.github.com>
2019-03-11 07:47:47 -0400

    Update app.py

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -21,6 +21,7 @@ def home():
 
 @app.route("/get_ip")
 def get_ip():
+    ip = "157.230.50.34"
     if request.args.get("ip") != None:
         try:
             ip = request.args.get("ip")
@@ -32,7 +33,7 @@ def get_ip():
             data = json.load(file)
             collection.insert_many(data)
         except:
-            ip = "104.248.114.55"
+            ip = "157.230.50.34"
             connection = pymongo.MongoClient(ip)
             connection.drop_database("database")
             db = connection["BoatBoatInspire"]
@@ -42,7 +43,7 @@ def get_ip():
             collection.insert_many(data)
     return render_template("search.html",ip=ip)
 
-ip = "104.248.114.55"
+ip = "157.230.50.34"
 connection = pymongo.MongoClient(ip)
 connection.drop_database("database")
 db = connection["BoatBoatInspire"]
@@ -52,7 +53,8 @@ data = json.load(file)
 collection.insert_many(data)
 
 def title_movie(title):
-    return collection.find({'title':title})
+    movies = collection.find({'title':title})
+    return movies
 
 def genre_movie(genre):
     return collection.find({'genres':genre})
@@ -62,12 +64,27 @@ def year_movie(year):
 
 @app.route("/query")
 def query():
-    results=[]
+    results = []
     q = request.args.get("search")
-    #results.append(title_movie(q))
-    results.append(genre_movie(q))
-    #results.append(year_movie(q))
-    return render_template("results.html",results=results[0])
+    try:
+        date = int(q)
+    except:
+        date = ""
+    new = title_movie(q)
+    for movie in new:
+        del movie["_id"]
+        if movie not in results:
+            results.append(movie)
+    new = genre_movie(q)
+    for movie in new:
+        if movie not in results:
+            results.append(movie)
+    new = year_movie(date)
+    for movie in new:
+        if movie not in results:
+            results.append(movie)
+    # print(results)
+    return render_template("results.html",results=results)
 
 app.debug = True
 app.run()
