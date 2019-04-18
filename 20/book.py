6c68b64721d3e13d9ed72d006e5c91e2016b1065
tlee8 <tlee8@stuy.edu>
2019-04-18 12:34:19 -0400

    listcomp

--- 20/book.py
+++ 20/book.py
@@ -1,14 +1,18 @@
 from functools import reduce
 
-words = open('book.txt', 'r').read().lower().strip(',.?!;:').split()
 
 #print(words)
 
 def wordfreq(word):
-    print ([1 for x in words if x == word])
-    #return len([1 for x in words if x == word])
+    words = open('book.txt', 'r').read().lower().strip(',.?!;:').split()
+    count = [1 for x in words if x == word]
+    return reduce((lambda x,y: x+y), count)
 
-print(wordfreq('torture'))
+print(wordfreq('peter'))
     
 
-def phrasefreq
+def phrasefreq(phrase):
+    words = open('book.txt', 'r').read().lower().strip(',.?!;:').split(phrase)
+    return len(words)-1
+
+#print(phrasefreq('peter pan'))
